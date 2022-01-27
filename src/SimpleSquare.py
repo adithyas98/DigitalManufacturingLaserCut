a9b9ff9053a3b01d354f3d72c15f07d6c49cb96d
sakib15499 <sakib15499@gmail.com>
2022-01-26 20:09:57 -0500

    updated code for first submission

--- src/SimpleSquare.py
+++ src/SimpleSquare.py
@@ -4,19 +4,30 @@
 This method will create a simple box with some text inside the box
 '''
 
-def square(w=10,h=10,text="Hello"):
+def square(w=10,h=10,text='AS and SA'):
     '''
     Inputs:
         - width: the width of the box (in cm)
         - height: the height of the box (in cm)
+        - length: the length of the box (in cm)
+        - thickness: thickness of material (in cm)
     Output:
         - outputs a svg file
     '''
     #Add the svg header info and define the viewport
     svg = '<svg viewBox="0 0 100cm 100cm" xmlns="http://www.w3.org/2000/svg">\n'
     #Add the square
-    svg += '<rect x="10" y="10" width="{}cm" height="{}cm"  />\n'.format(str(w),str(h))
+    svg += '<rect x="0.1cm" y="0.1cm" width="{}cm" height="{}cm" style="stroke: #009900; stroke-width: 3; fill:none;"/>\n'.format(str(w),str(h))
     #Add text
-    svg += '<text x={}, y={},
-
-
+    svg += '<text x="{}cm" y="{}cm" font-size="1cm">{}</text>\n'.format(str(0.1+w/4),str(0.1+h/2),text)
+    
+    #End the svg file
+    svg += '</svg>'
+    #return the output
+    return svg
+    
+    
+if __name__ == "__main__":
+    with open("square.svg","w") as f:
+        f.write(square())
+        
