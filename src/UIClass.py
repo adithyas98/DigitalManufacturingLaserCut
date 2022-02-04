e15d2997ec6f07745b59a946a00fd067cc8d1bec
Adithya Shastry <adi.shastry@gmail.com>
2022-02-03 21:17:31 -0500

    Created the UI class

--- src/UIClass.py
+++ src/UIClass.py
@@ -1,8 +1,62 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 31 21:46:17 2022
-
-@author: sakib
-"""
+#!/usr/bin/env python3
 
 
+class UserInput:
+    '''
+    Class will define methods to accept user input
+    '''
+    def __init__(self,qdict,intro):
+        '''
+        Input:
+            - dictionary: A dictionary of inputs to ask the user
+                - The dictionary should take the following form
+                    Variable Code(code to access the answer)
+                       (Create a sub dictionary)
+                        - Question: The Question to ask
+                        - Data Type: pass in the conversion function
+                                    pointer
+            - intro: What you would like to start the questioning with
+        '''
+        self.intro = intro
+        self.qdict = qdict
+        self.fullDict = qdict  #Used once we have our answers
+
+    def askQuestions(self):
+        '''
+        This method will ask the questions defined in the qdata
+        Output:
+            - the qdata dictionary with a data label containing the data
+            as follows:
+                    Variable Code(code to access the answer)
+                       (Create a sub dictionary)
+                        - Question: The Question to ask
+                        - convert: pass in the conversion function
+                                    pointer
+                        - data: Contains the data
+        '''
+        #Print out the intro paragraph
+        print("{}\n".format(self.intro))
+        #iterate through the keys and ask the questions
+        for key in qdict.keys():
+            #find the conversion function
+            convert = qdict[key]['convert']
+            ans = input(qdict[key]["question"])#ask the question
+            #Now we need to convert the data then add it to the dict
+            fullDict[key]['data'] = convert(ans)
+
+        #once we have done this for everything, we can return fullData
+        return self.fullData
+
+    ##TODO: Create a method to output a text file of questions
+    ##TODO: Create a method to input answers from the text file
+
+
+
+
+
+
+        
+
+        
+
+        
